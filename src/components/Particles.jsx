e3e2e2870be659f669ec491f791bcc74efcd53d5
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-08-06 12:25:58 +0530

    Refactor Particles component to use dynamic particle data mapping

--- src/components/Particles.jsx
+++ src/components/Particles.jsx
@@ -1,26 +1,26 @@
 function Particles() {
+  // Particle configuration data
+  const particleData = [
+    { size: "w-2 h-2", top: "20%", left: "10%", delay: "0s" },
+    { size: "w-1 h-1", top: "60%", left: "80%", delay: "2s" },
+    { size: "w-3 h-3", top: "80%", left: "20%", delay: "4s" },
+    { size: "w-1 h-1", top: "30%", left: "70%", delay: "1s" },
+    { size: "w-2 h-2", top: "70%", left: "50%", delay: "3s" }
+  ];
+
   return (
     <div className="fixed inset-0 overflow-hidden pointer-events-none">
-      <div
-        className="particle w-2 h-2"
-        style={{ top: "20%", left: "10%", animationDelay: "0s" }}
-      ></div>
-      <div
-        className="particle w-1 h-1"
-        style={{ top: "60%", left: "80%", animationDelay: "2s" }}
-      ></div>
-      <div
-        className="particle w-3 h-3"
-        style={{ top: "80%", left: "20%", animationDelay: "4s" }}
-      ></div>
-      <div
-        className="particle w-1 h-1"
-        style={{ top: "30%", left: "70%", animationDelay: "1s" }}
-      ></div>
-      <div
-        className="particle w-2 h-2"
-        style={{ top: "70%", left: "50%", animationDelay: "3s" }}
-      ></div>
+      {particleData.map((particle, index) => (
+        <div
+          key={index}
+          className={`particle ${particle.size}`}
+          style={{ 
+            top: particle.top, 
+            left: particle.left, 
+            animationDelay: particle.delay 
+          }}
+        ></div>
+      ))}
     </div>
   );
 }
